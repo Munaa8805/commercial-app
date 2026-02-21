395af30a0c0d49c7345c40cbe3bb8b258631d504
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2026-02-21 00:15:39 -0700

    fix it

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
-import { ProductProvider } from "./context/productContext";
+import { ProductProvider } from "./context/ProductContext";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProductList from "./components/ProductList";
